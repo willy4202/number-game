c398b61bd26c308c1cd949970d4b7980c84def68
Young <willy4202@naver.com>
2023-11-22 21:12:15 +0900

    add : 게임오버로직 추가

--- App.js
+++ App.js
@@ -5,17 +5,30 @@ import StartGameScreen from "./screens/StartGameScreen";
 import theme from "./styles/theme";
 import { useState } from "react";
 import GameScreen from "./screens/GameScreen";
+import GameOverScreen from "./screens/GameOverScreen";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setGameIsOver] = useState(true);
+
   const userNumberHandler = (number) => {
     setUserNumber(number);
+    setGameIsOver(false);
+  };
+  const gameOverHandler = () => {
+    setGameIsOver(true);
   };
 
   let screen = <StartGameScreen numberHandler={userNumberHandler} />;
 
   if (userNumber) {
-    screen = <GameScreen userNumber={userNumber} />;
+    screen = (
+      <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
+    );
+  }
+
+  if (gameIsOver && userNumber) {
+    screen = <GameOverScreen />;
   }
 
   return (
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import Title from "../components/ui/Title";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import NumberContainer from "../components/game/NumberContainer";
@@ -12,17 +12,64 @@ const generateRandomBetween = (min, max, exlcude) => {
     return rndNum;
   }
 };
-const GameScreen = ({ userNumber }) => {
+
+let minBoundary = 1;
+let maxBoudnary = 100;
+
+const GameScreen = ({ userNumber, onGameOver }) => {
   const initalGuess = generateRandomBetween(1, 100, userNumber);
 
   const [currentGuess, setCureentGuess] = useState(initalGuess);
 
+  useEffect(() => {
+    if (currentGuess === userNumber) {
+      onGameOver();
+    }
+  }, [currentGuess, userNumber, onGameOver]);
+
+  const nextGuessHandler = (direction) => {
+    if (
+      (direction === "lower" && currentGuess < userNumber) ||
+      (direction === "greater" && currentGuess > userNumber)
+    ) {
+      Alert.alert("거짓말입니다.", "잘못된 정보를 알려줬습니다.", [
+        { text: "다시하기", style: "cancel" },
+      ]);
+      return;
+    }
+
+    if (direction === "lower") {
+      maxBoudnary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+
+    const newRndNumber = generateRandomBetween(
+      minBoundary,
+      maxBoudnary,
+      currentGuess
+    );
+    setCureentGuess(newRndNumber);
+  };
+
   return (
     <View style={styles.screen}>
       <Title>예상 숫자</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higer or Lower?</Text>
-        <NumberContainer>{currentGuess}</NumberContainer>
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+              -
+            </PrimaryButton>
+          </View>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+              +
+            </PrimaryButton>
+          </View>
+        </View>
       </View>
       <View>
         <Text>Log Rounds</Text>
@@ -37,5 +84,11 @@ export default GameScreen;
 const styles = StyleSheet.create({
   screen: {
     padding: 24,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
