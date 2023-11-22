08906ec11ebbd2c7ca3e82d82d3a601bc041f110
Young <willy4202@naver.com>
2023-11-22 15:35:33 +0900

    btn ripple 적용

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,16 +1,51 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import theme from "../styles/theme";
 
 const PrimaryButton = ({ children }) => {
+  const pressHandler = () => {};
   return (
-    <Pressable>
-      <View>
-        <Text>{children}</Text>
-      </View>
-    </Pressable>
+    <View style={styles.outerContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.pressed, styles.innerContainer]
+            : styles.innerContainer
+        }
+        android_ripple={{ color: theme.blue600 }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
   );
 };
 
 export default PrimaryButton;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  outerContainer: {
+    borderRadius: 32,
+    margin: 4,
+    overflow: "hidden",
+  },
+  innerContainer: {
+    backgroundColor: theme.blue500,
+
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+
+    elevation: 2,
+    shadowColor: "#00000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.15,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,11 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import React from "react";
 import PrimaryButton from "../components/PrimaryButton";
+import theme from "../styles/theme";
 
 const StartGameScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>StartGameScreen</Text>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoComplete="off"
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -16,8 +22,28 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     marginTop: 100,
+    marginHorizontal: 16,
     padding: 16,
+    backgroundColor: theme.blue700,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "#00000",
+    shadowOffset: { width: 4, height: 4 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+
+  numberInput: {
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: theme.yellow300,
+    borderBottomWidth: 2,
+    color: theme.yellow300,
+    marginVertical: 8,
+    fontWeight: "bold",
+    width: 50,
+    textAlign: "center",
   },
 });
